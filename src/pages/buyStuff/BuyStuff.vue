c122dea4e464aa6cb0c11d5a3e845b9f5f2aa508
ciel-cc <cielcmh@163.com>
2020-04-09 18:52:36 +0800

    fix：购买页面交易方式

--- src/pages/buyStuff/BuyStuff.vue
+++ src/pages/buyStuff/BuyStuff.vue
@@ -2,16 +2,37 @@
   <div class="buy_stuff">
     <StuffTop msg="购买宝贝" />
     <StuffItem/>
-    <StuffAddressBar />
-    <div class="stuff_carry">
+<!--    选择物品交易方式-->
+    <div class="post_select">
       <van-row>
-        <van-col span="5">
-          <span>运费</span>
+        <van-col span="9">
+          <span>请选择物品交易方式</span>
         </van-col>
-        <van-col span="4" align="center" offset="15" class="cast_text">
-          12.00
+        <van-col span="14" align="center" offset="1" class="cast_text">
+          <van-radio-group v-model="postmethod" direction="horizontal">
+            <van-radio name="1" icon-size="18px">线下交易</van-radio>
+            <van-radio name="2" icon-size="18px">邮寄</van-radio>
+          </van-radio-group>
         </van-col>
       </van-row>
+    </div>
+<!--    邮寄-->
+    <div v-show="postmethod==2">
+      <StuffAddressBar />
+      <div class="stuff_carry">
+        <van-row>
+          <van-col span="5">
+            <span>运费</span>
+          </van-col>
+          <van-col span="4" align="center" offset="15" class="cast_text">
+            {{freight}}
+          </van-col>
+        </van-row>
+      </div>
+    </div>
+<!--    线下-->
+    <div v-show="postmethod==1">
+      <ProviderAddress />
     </div>
     <div class="pay_bar">
       <van-submit-bar
@@ -27,16 +48,39 @@
   import StuffItem from './component/StuffItem'
   import StuffTop from '../../components/TopReturnBar'
   import StuffAddressBar from './component/StuffAddressBar'
+  import ProviderAddress from './component/ProviderAddress'
   import Vue from 'vue';
   import { Panel } from 'vant';
   import { SubmitBar } from 'vant';
+  import { RadioGroup, Radio } from 'vant';
 
+  Vue.use(Radio);
+  Vue.use(RadioGroup);
   Vue.use(SubmitBar);
   Vue.use(Panel);
     export default {
       name: "BuyStuff",
       components: {
-        StuffItem, StuffTop, StuffAddressBar
+        StuffItem, StuffTop, StuffAddressBar, ProviderAddress
+      },
+      data() {
+        return {
+          title: '',
+          detail: '',
+          unitprice:'',
+          stuffprice: '',
+          totalprice: '',
+          //运费
+          freight: '0.00',
+          //押金
+          deposit: '',
+          name: '',
+          address: '',
+          phone: '',
+          postmethod:'',
+          proId: '',
+          buyerUserId: '',
+        }
       },
     }
 </script>
@@ -46,7 +90,7 @@
   height: 100%;
   background-color: #f1f1f1;
 }
-  .stuff_carry{
+  .stuff_carry, .post_select{
     padding: 1rem 9px;
     background-color: #fafafa;
     margin: 4px 0;
@@ -54,5 +98,8 @@
   }
   .cast_text{
     color: #de4646;
+  }
+  .post_select{
+    font-size: small;
   }
 </style>
---
+++ src/pages/buyStuff/component/ProviderAddress.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="provider_addrss">
+      <div>姓名：{{name}}</div>
+      <div>手机号：{{phone}}</div>
+      <div>地址：{{address}}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProviderAddress",
+      data() {
+          return {
+            name: 'Tony',
+            phone: '10086',
+            address: 'address'
+          }
+      }
+    }
+</script>
+
+<style scoped>
+.provider_addrss{
+  background-color: #fafafa;
+  color: #87898a;
+  padding: 1rem 9px;
+  margin: 4px 0;
+  line-height: 1.5;
+}
+</style>
