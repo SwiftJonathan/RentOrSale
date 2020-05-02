f0d54378ac7d7f824e2cd2d051bbd58663761547
Littledad <13340118943@163.com>
2020-05-02 22:48:50 +0800

    发布页面ui更改

--- src/pages/publish/Publish.vue
+++ src/pages/publish/Publish.vue
@@ -5,74 +5,97 @@
     </div>
 
     <div class="publish-type">
-      <div class="title" @click="handlerRBClick('0')">I Wana Sale !</div>
-      <div class="description">Here some desctiption to sale.</div>
-    </div>
-
-    <div class="publish-type-sub">
-      <div class="title" @click="handlerRBClick('1')">I Want Sale !</div>
-      <div class="description">Here some desctiption to sale.</div>
-    </div>
-
-    <van-form id="formCon" @submit="onSubmit"  enctype="multipart/form-data">
       <div class="title">
-        <van-field v-model="name" label="发布标题" placeholder="请输入发布标题" maxlength="20" name="name" required
-                   :rules="[{ required: true, message: '请填写标题' }]"/>
-      </div>
-
-      <div class="description">
-        <van-field
-          v-model="detail"
-          rows="3"
-          :autosize=areaHeight
-          label="商品描述"
-          type="textarea"
-          maxlength="255"
-          placeholder="请输入商品描述"
-          show-word-limit
-          name="detail"
-          required
-          :rules="[{ required: true, message: '请填写描述' }]"
-        />
-      </div>
-      <div class="price">
-        <van-field v-model="price" type="number" label="价格" placeholder="请输入发布价格" name="price" required
-                   :rules="[{ required: true, message: '请填写价格' }]"/>
-      </div>
-      <div class="freight">
-        <van-field v-model="freight" type="digit" label="运费" placeholder="请输入发布运费" name="freight" />
-      </div>
-      <div class="deposit" v-show="SaleOrRent ==='1'">
-        <van-field v-model="deposit" type="digit" label="押金" placeholder="请输入押金" name="deposit" />
-      </div>
-
-
-      <div class="uploader">
-        <van-uploader v-model="fileList" multiple name="fileList" />
-      </div>
-
-      <div>
-        <van-field type="hidden" name="providerUserId" v-model="providerUserId" />
-        <van-field type="hidden" name="rentPrice" v-model="price" />
-      </div>
-      <div class="submit">
-        <div class="button">
-          <button type="submit">Publish</button>
+        <div class="show">
+          <span>I Wana {{SaleOrRent === "0" ? "Sale" : "Rent"}} !</span>
+        </div>
+        <div class="unshow" @click="handlerRBClick">
+          <span>I Wana {{SaleOrRent === "1" ? "Sale" : "Rent"}} !</span>
         </div>
       </div>
-    </van-form>
+      <div class="description">Here some desctiption to sale.</div>
+    </div>
+
+    <div class="form">
+      <van-form id="formCon" @submit="onSubmit" enctype="multipart/form-data">
+        <div class="title form-item">
+          <van-field
+            v-model="name"
+            label="发布标题"
+            placeholder="请输入发布标题"
+            maxlength="20"
+            name="name"
+            required
+            :rules="[{ required: true, message: '请填写标题' }]"
+          />
+        </div>
+
+        <div class="description form-item">
+          <van-field
+            v-model="detail"
+            rows="3"
+            :autosize="areaHeight"
+            label="商品描述"
+            type="textarea"
+            maxlength="255"
+            placeholder="请输入商品描述"
+            show-word-limit
+            name="detail"
+            required
+            :rules="[{ required: true, message: '请填写描述' }]"
+          />
+        </div>
+        <div class="price form-item">
+          <van-field
+            v-model="price"
+            type="number"
+            label="价格"
+            placeholder="请输入发布价格"
+            name="price"
+            required
+            :rules="[{ required: true, message: '请填写价格' }]"
+          />
+        </div>
+        <div class="freight form-item">
+          <van-field
+            v-model="freight"
+            type="digit"
+            label="运费"
+            placeholder="请输入发布运费"
+            name="freight"
+          />
+        </div>
+        <div class="deposit form-item" v-show="SaleOrRent ==='1'">
+          <van-field v-model="deposit" type="digit" label="押金" placeholder="请输入押金" name="deposit" />
+        </div>
+
+        <div class="uploader form-item">
+          <van-uploader v-model="fileList" multiple name="fileList" />
+        </div>
+
+        <div class="hidden">
+          <van-field type="hidden" name="providerUserId" v-model="providerUserId" />
+          <van-field type="hidden" name="rentPrice" v-model="price" />
+        </div>
+
+        <div class="submit form-item">
+          <div class="button">
+            <button type="submit">Publish</button>
+          </div>
+        </div>
+      </van-form>
+    </div>
   </div>
 </template>
 
 
 <script>
 // import axios from 'axios';
-import {HTTP_URL} from '@/store/const.js'
+import { HTTP_URL } from "@/store/const.js";
 import Vue from "vue";
 import { Field, Uploader, CheckboxGroup, Checkbox, Toast } from "vant";
 import { Form } from "vant";
 import axios from "axios";
-
 
 Vue.use(Form);
 Vue.use(Field);
@@ -113,7 +136,7 @@ export default {
         console.log("file", element.file);
         data.append("fileList", element.file, element.file.name);
       });
-      for (let key of data.getAll("fileList")){
+      for (let key of data.getAll("fileList")) {
         console.log("data.fileLIst in for", key);
       }
       for (let key in values) {
@@ -123,11 +146,12 @@ export default {
         console.log(key, data.getAll(key));
       }
       let req_url = `${HTTP_URL}/pro/addSell`;
-      if (this.SaleOrRent === '1'){
+      if (this.SaleOrRent === "1") {
         data.append("rentPrice", this.price);
         req_url = `${HTTP_URL}/pro/addRent`;
       }
-      axios.post(req_url, data, {
+      axios
+        .post(req_url, data, {
           headers: {
             "Content-Type": "multipart/form-data"
           }
@@ -145,12 +169,10 @@ export default {
           }
         );
     },
-    handlerRBClick(val){
-      this.SaleOrRent = val;
-      console.log("this.saleorrent", this.SaleOrRent);
+    handlerRBClick(val) {
+      this.SaleOrRent = this.SaleOrRent === "0" ? "1" : "0";
     },
-    handlerClearForm(){
-      console.log("clear");
+    handlerClearForm() {
       this.name = "";
       this.price = "";
       this.freight = "";
@@ -183,64 +205,48 @@ export default {
   padding: 0px 28px;
 }
 .publish > .publish-type > .title {
+  display: flex;
+  justify-content: flex-start;
+}
+.publish > .publish-type > .title > .show {
   font-size: 24px;
   font-weight: 800;
   color: rgba(22, 23, 55, 1);
+  display: flex;
+  align-items: flex-end;
+}
+.publish > .publish-type > .title > .unshow {
+  margin-left: 20px;
+  font-size: 16px;
+  font-weight: 500;
+  color: orangered;
+  display: flex;
+  align-items: flex-end;
 }
 .publish > .publish-type > .description {
   color: rgba(0, 0, 0, 0.5);
 }
 
-.publish > .publish-type-sub {
-  padding: 0px 28px;
-}
-.publish > .publish-type-sub > .title {
-  font-size: 16px;
-  font-weight: 600;
-  color: rgba(233, 147, 136, 1);
-}
-.publish > .publish-type-sub > .description {
-  color: rgba(0, 0, 0, 0.5);
-  margin-bottom: 14px;
+.form {
+  padding: 12px 28px;
 }
 
-.publish > .title {
-  padding: 0px 12px;
-  margin-bottom: 4px;
-}
-
-.publish > .description {
-  padding: 0px 12px;
-  margin-bottom: 4px;
-}
-
-.publish > .price {
-  padding: 0px 12px;
-  margin-bottom: 4px;
-}
-
-.publish > .freight {
-  padding: 0px 12px;
+.form-item {
   margin-bottom: 8px;
 }
 
-.publish > .uploader {
-  padding: 0px 12px;
-  margin-bottom: 4px;
+.hidden{
+  visibility: hidden;
 }
-
-.submit_btn {
-  position: absolute;
-  bottom: 15vh;
-  width: 94vw;
-  left: 3vw;
+.button{
+  background-color: orangered;
   height: 60px;
-  background-color: rgba(22, 23, 55, 1);
+  border-radius: 4px;
   display: flex;
   justify-content: center;
-}
-.submit_btn > .button {
-  /* margin: 0px 28px; */
-  width: 80vw;
+  align-items: center;
+  color: rgba(240, 240, 240, 1);
+  font-weight: 600;
+  font-size: 20px;
 }
 </style>
